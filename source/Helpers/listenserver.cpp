e80ff34df4b8384842dbe1ba312411bb72c0ca93
Savenkov Igor <savenkovigor.freelance@gmail.com>
2018-07-22 15:49:10 +0900

    Fill listenServer

--- source/Helpers/listenserver.cpp
+++ source/Helpers/listenserver.cpp
@@ -1,6 +1,32 @@
 #include "listenserver.h"
 
-ListenServer::ListenServer(QObject *parent) : QObject(parent)
+ListenServer::ListenServer(quint16 listenPort, QObject *parent) : QObject(parent)
 {
+    this->listenPort = listenPort;
+    server = new QTcpServer(this);
+    connect(server, SIGNAL(newConnection()), SLOT(newConnection()));
+}
 
+bool ListenServer::startListen()
+{
+    return server->listen(QHostAddress::Any, listenPort);
+}
+
+void ListenServer::newConnection()
+{
+    if (server->hasPendingConnections()) {
+        QTcpSocket* newSocket = server->nextPendingConnection();
+        connect(newSocket, SIGNAL(readyRead()), SLOT(newInputData()));
+        this->inputSockets.append(newSocket);
+    }
+}
+
+void ListenServer::newInputData()
+{
+    QTcpSocket *inputSocket = dynamic_cast<QTcpSocket*>(sender());
+    if (inputSocket == nullptr)
+        return;
+    QHostAddress senderAddress = inputSocket->peerAddress();
+    QByteArray data = inputSocket->readAll();
+    newReceivedData(senderAddress, data);
 }
--- source/Helpers/listenserver.h
+++ source/Helpers/listenserver.h
@@ -3,18 +3,28 @@
 
 #include <QObject>
 #include <QTcpServer>
+#include <QTcpSocket>
 
+/// Simple server, that can work with only one connection
 class ListenServer : public QObject
 {
     Q_OBJECT
 public:
-    explicit ListenServer(QObject *parent = nullptr);
-
+    explicit ListenServer(quint16 listenPort, QObject *parent = nullptr);
 signals:
 
 public slots:
+    bool startListen();
+    void newReceivedData(QHostAddress fromHost, QByteArray data);
+
+private slots:
+    void newConnection();
+    void newInputData();
+
 private:
     QTcpServer* server;
+    QList<QTcpSocket*> inputSockets;
+    quint16 listenPort;
 };
 
 #endif // LISTENSERVER_H
