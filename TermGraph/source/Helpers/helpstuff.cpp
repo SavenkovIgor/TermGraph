8c6a910668505e55013f26b850e616816f631418
Savenkov Igor <savenkovigor.freelance@gmail.com>
2018-11-04 11:36:29 +0900

    Extend help stuff with size debug and rect debug

--- TermGraph/source/Helpers/helpstuff.cpp
+++ TermGraph/source/Helpers/helpstuff.cpp
@@ -1,13 +1,38 @@
 #include "helpstuff.h"
 
-QString HelpStuff::ptToStr(const int& x, const int& y, const QString& divider) {
-    return QString::number(x) + divider + QString::number(y);
+QString HelpStuff::ptToStr(const int& x, const int& y) {
+    return "point x:" + QString::number(x) + " y:" + QString::number(y);
 }
 
-QString HelpStuff::ptToStr(const QPoint& pt, const QString& divider) {
-    return ptToStr(pt.x(), pt.y(), divider);
+QString HelpStuff::ptToStr(const QPoint& pt) {
+    return ptToStr(pt.x(), pt.y());
 }
 
-QString HelpStuff::ptToStr(const QPointF& pt, const QString& divider) {
-    return ptToStr(pt.toPoint(), divider);
+QString HelpStuff::ptToStr(const QPointF& pt) {
+    return ptToStr(pt.toPoint());
+}
+
+QString HelpStuff::sizeToStr(const int &width, const int &height)
+{
+    return "size w:" + QString::number(width) + " h:" + QString::number(height);
+}
+
+QString HelpStuff::sizeToStr(const QSize &size)
+{
+    return sizeToStr(size.width(), size.height());
+}
+
+QString HelpStuff::sizeToStr(const QSizeF &size)
+{
+    return sizeToStr(size.toSize());
+}
+
+QString HelpStuff::rectToStr(const QRect &rect)
+{
+    return ptToStr(rect.topLeft()) + " " + sizeToStr(rect.size());
+}
+
+QString HelpStuff::rectToStr(const QRectF &rect)
+{
+    return rectToStr(rect.toRect());
 }
--- TermGraph/source/Helpers/helpstuff.h
+++ TermGraph/source/Helpers/helpstuff.h
@@ -2,14 +2,22 @@
 #define HELPSTUFF_H
 
 #include <QString>
-#include <QPoint>
 #include <QPointF>
+#include <QSizeF>
+#include <QRectF>
 
 class HelpStuff {
 public:
-    static QString ptToStr(const int& x, const int& y, const QString& divider = "x");
-    static QString ptToStr(const QPoint& pt, const QString& divider = "x");
-    static QString ptToStr(const QPointF& pt, const QString& divider = "x");
+    static QString ptToStr(const int& x, const int& y);
+    static QString ptToStr(const QPoint& pt);
+    static QString ptToStr(const QPointF& pt);
+
+    static QString sizeToStr(const int& width, const int& height);
+    static QString sizeToStr(const QSize& size);
+    static QString sizeToStr(const QSizeF& size);
+
+    static QString rectToStr(const QRect& rect);
+    static QString rectToStr(const QRectF& rect);
 };
 
 #endif // HELPSTUFF_H
