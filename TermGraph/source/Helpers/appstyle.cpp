0cef779b16af91524687bd0a1dc26526941df0f7
Savenkov Igor <lunaticrl42@gmail.com>
2018-11-22 12:49:15 +0900

    Replace color Hex codes to int numbers

--- TermGraph/source/Helpers/appstyle.cpp
+++ TermGraph/source/Helpers/appstyle.cpp
@@ -4,13 +4,13 @@
 QColor AppStyle::Colors::Nodes::text = QColor(255, 255, 255);
 QColor AppStyle::Colors::Nodes::border = QColor(0, 0, 0);
 
-QColor AppStyle::Colors::Nodes::leaf = QColor("#6d9a28");
-QColor AppStyle::Colors::Nodes::root = QColor("#802915");
-QColor AppStyle::Colors::Nodes::orphan = QColor("#124b6a");
+QColor AppStyle::Colors::Nodes::leaf = QColor(109, 154, 40);
+QColor AppStyle::Colors::Nodes::root = QColor(128, 41, 21);
+QColor AppStyle::Colors::Nodes::orphan = QColor(18, 75, 106);
 
-QColor AppStyle::Colors::Nodes::leafSelected = QColor("#83c31b");
-QColor AppStyle::Colors::Nodes::rootSelected = QColor("#dc4c1d");
-QColor AppStyle::Colors::Nodes::orphanSelected = QColor("#0b77b1");
+QColor AppStyle::Colors::Nodes::leafSelected = QColor(131, 195, 27);
+QColor AppStyle::Colors::Nodes::rootSelected = QColor(220, 76, 29);
+QColor AppStyle::Colors::Nodes::orphanSelected = QColor(11, 119, 177);
 
 QColor AppStyle::Colors::Nodes::atLearn = QColor(245, 222, 179);
 
@@ -20,11 +20,11 @@ QColor AppStyle::Colors::Groups::backgroundTerms = QColor(0, 0, 0, 0);
 QColor AppStyle::Colors::Groups::backgroundFreeConnections = QColor(0, 0, 0, 0);
 
 QColor AppStyle::Colors::Edges::standard = QColor(Qt::black);
-QColor AppStyle::Colors::Edges::termin = QColor("#225b7a");
+QColor AppStyle::Colors::Edges::termin = QColor(34, 91, 122);
 QColor AppStyle::Colors::Edges::description = QColor(Qt::green);
 QColor AppStyle::Colors::Edges::selected = QColor(0, 166, 147);
 
-QColor AppStyle::Colors::Scene::background = QColor("#332f30");
+QColor AppStyle::Colors::Scene::background = QColor(51, 47, 48);
 
 QColor AppStyle::Colors::transparent = QColor(0,0,0,0);
 QColor AppStyle::Colors::testColor = QColor(0, 255, 0);
