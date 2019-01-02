66e0b0c5b4a40e71e166c2d395cd5c78927cc14a
Savenkov Igor <savenkovigor.freelance@gmail.com>
2019-01-02 15:32:34 +0900

    Add graphicItem class

--- TermGraph/source/graphicitem.cpp
+++ TermGraph/source/graphicitem.cpp
@@ -2,5 +2,23 @@
 
 GraphicItem::GraphicItem()
 {
+}
 
+void GraphicItem::setPos(QPointF pt)
+{
+    position = pt;
+}
+
+QPointF GraphicItem::pos() const
+{
+    return position;
+}
+
+QPointF GraphicItem::scenePos() const
+{
+    if (parentItem == nullptr) {
+        return position;
+    }
+
+    return position + parentItem->scenePos();
 }
--- TermGraph/source/graphicitem.h
+++ TermGraph/source/graphicitem.h
@@ -1,10 +1,23 @@
 #ifndef GRAPHICITEM_H
 #define GRAPHICITEM_H
 
+#include <QPointF>
+
 class GraphicItem
 {
 public:
     GraphicItem();
+
+    // Parents
+    void setParentItem(GraphicItem* item);
+
+    // Position
+    void setPos(QPointF pt);
+    QPointF pos() const;
+    QPointF scenePos() const;
+
+private:
+    QPointF position;
 
     GraphicItem* parentItem = nullptr;
 };
