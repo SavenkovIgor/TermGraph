1fcaefb22ea333fa6131955af4ecf08fb4a1c75c
SavenkovIgor <lunaticrl42@gmail.com>
2019-02-27 13:04:58 +0900

    Some new fixes to app config table

--- TermGraph/source/databaseWorks/appconfigtable.cpp
+++ TermGraph/source/databaseWorks/appconfigtable.cpp
@@ -3,12 +3,10 @@
 void AppConfigTable::initTable()
 {
     createTable();
+    // Add database version parameter
+    addValue(dbVersionPropertyName, dbVersion);
 
-    QList<InsertContainer> values;
-    values << InsertContainer(AppConfigColumn::parameter, dbVersionPropertyName);
-    values << InsertContainer(AppConfigColumn::value, dbVersion);
-
-    insertInto(values);
+    getValue(dbVersionPropertyName, 10);
 }
 
 TColumn::List AppConfigTable::getAllColumns() const
@@ -26,4 +24,53 @@ void AppConfigTable::insertVersionFieldIfNeed()
     auto where = WhereCondition();
     where.equal(AppConfigColumn::parameter, dbVersionPropertyName);
     toRecList(select(AppConfigColumn::value, where));
+}
+
+void AppConfigTable::addValue(const QString &key, const int &value)
+{
+    addValue(key, QString::number(value));
+}
+
+void AppConfigTable::addValue(const QString &key, const QString &value)
+{
+    QList<InsertContainer> values;
+
+    values << InsertContainer(AppConfigColumn::parameter, key);
+    values << InsertContainer(AppConfigColumn::value, value);
+
+    insertInto(values);
+}
+
+bool AppConfigTable::hasKey(const QString &key)
+{
+    auto where = WhereCondition();
+    where.equal(AppConfigColumn::parameter, key);
+    auto recList = toRecList(select(AppConfigColumn::parameter, where));
+    return recList.size() == 1;
+}
+
+int AppConfigTable::getValue(const QString &key, const int &defaultValue)
+{
+    auto strValue = getValue(key, QString::number(defaultValue));
+    bool* castOk = new bool(false);
+    auto result = strValue.toInt(castOk);
+    if (castOk) {
+        delete castOk;
+        return result;
+    }
+    delete castOk;
+    return defaultValue;
+}
+
+QString AppConfigTable::getValue(const QString &key, const QString &defaultValue)
+{
+    if (hasKey(key)) {
+        auto where = WhereCondition();
+        where.equal(AppConfigColumn::parameter, key);
+        auto recList = toRecList(select(AppConfigColumn::value, where));
+        return recList.first().value(AppConfigColumn::value).toString();
+    }
+
+    addValue(key, defaultValue);
+    return defaultValue;
 }
--- TermGraph/source/databaseWorks/appconfigtable.h
+++ TermGraph/source/databaseWorks/appconfigtable.h
@@ -22,6 +22,7 @@ public:
     AppConfigTable(QSqlDatabase* base): TblBase(TableName::CONFIG, base) { }
     ~AppConfigTable() override = default;
 
+    // This function must be called only once, when initing database
     void initTable();
 
     bool isDbNeedUpdate();
@@ -34,6 +35,15 @@ private:
     constexpr static auto dbVersion = 1;
 
     void insertVersionFieldIfNeed();
+
+    // Values works
+    void addValue(const QString& key, const int& value);
+    void addValue(const QString& key, const QString& value);
+
+    bool hasKey(const QString& key);
+
+    int     getValue(const QString& key, const int& defaultValue = -1);
+    QString getValue(const QString& key, const QString& defaultValue = "");
 };
 
 #endif // APPCONFIGTABLE_H
