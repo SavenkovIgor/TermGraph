6eceb523379b647bde925d9fe4a05e9114efada2
Savenkov Igor <savenkovigor.freelance@gmail.com>
2019-10-27 21:33:50 +0300

    Remove utf bom

--- TermGraph/source/databaseWorks/ndtbl.cpp
+++ TermGraph/source/databaseWorks/ndtbl.cpp
@@ -19,7 +19,7 @@
  *  along with TermGraph. If not, see <https://www.gnu.org/licenses/>.
  */
 
-﻿#include "source/databaseWorks/ndtbl.h"
+#include "source/databaseWorks/ndtbl.h"
 
 #include "source/databaseWorks/columns/nodecolumn.h"
 
