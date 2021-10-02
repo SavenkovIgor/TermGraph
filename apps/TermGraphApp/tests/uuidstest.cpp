f06c57ebac334e4b227d8f1911ea1f63afa7fc32
Igor Savenkov <savenkovigor.dev@gmail.com>
2021-10-03 02:23:16 +0300

    Add uuid classes

--- apps/TermGraphApp/tests/uuidstest.cpp
+++ apps/TermGraphApp/tests/uuidstest.cpp
@@ -22,6 +22,9 @@
 #include <gtest/gtest.h>
 
 #include "source/helpers/uuid/uuidtools.h"
+#include "source/model/uuid/groupuuid.h"
+#include "source/model/uuid/safeuuid.h"
+#include "source/model/uuid/termuuid.h"
 
 TEST(UuidsTest, Validator)
 {
@@ -45,4 +48,30 @@ TEST(UuidsTest, NoBracesValidator)
     EXPECT_FALSE(UuidTools::isValidUuidStringWithoutBraces(" 999-999 "));
     EXPECT_FALSE(UuidTools::isValidUuidStringWithoutBraces("{}"));
     EXPECT_FALSE(UuidTools::isValidUuidStringWithoutBraces(""));
+}
+
+TEST(UuidsTest, SafeTest)
+{
+    auto nullUuid = QUuid();
+    auto someUuid = QUuid::createUuid();
+
+    auto safe1 = SafeUuid::create(nullUuid.toString());
+    EXPECT_FALSE(safe1.has_value());
+
+    auto safe2 = SafeUuid::create("");
+    EXPECT_FALSE(safe2.has_value());
+
+    auto safe3 = SafeUuid::create(someUuid.toString());
+    EXPECT_TRUE(safe3.has_value());
+    EXPECT_EQ(safe3->toString(), someUuid.toString());
+    EXPECT_EQ(safe3->get(), someUuid);
+
+    auto str   = QString("94810de3-51b8-469e-b316-00248ffa2a45");
+    auto safe4 = SafeUuid::create(str);
+    EXPECT_TRUE(safe4.has_value());
+    EXPECT_EQ(safe4->toString(), "{" + str + "}");
+
+    // Must be compilable
+    TermUuid  termU  = TermUuid::create(str).value();
+    GroupUuid groupU = GroupUuid::create(str).value();
 }
