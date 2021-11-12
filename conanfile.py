5fd501f03d3177b88403e2ede4f0aa7870166541
Igor Savenkov <savenkovigor.dev@gmail.com>
2021-11-13 01:16:08 +0300

    Fix backend build rule

--- conanfile.py
+++ conanfile.py
@@ -7,9 +7,11 @@ class TermGraphConan(ConanFile):
 
     requires = 'gtest/1.10.0', 'restinio/0.6.13', 'outcome/2.2.1'
 
-    options = {'with_qt': [True, False]}
+    options = {'with_qt':       [True, False],
+               'build_backend': [True, False]}
 
     default_options = {'with_qt': False,
+                       'build_backend': False,
                        'qt:qtdeclarative': True,
                        'qt:qtquickcontrols2': True,
                        'qt:qtsvg': True,
@@ -18,6 +20,9 @@ class TermGraphConan(ConanFile):
     def requirements(self):
         if self.options.with_qt:
             self.requires('qt/6.2.1')
+
+        if self.options.build_backend:
+            self.requires('restinio/0.6.13')
 
     def build(self):
         cmake = CMake(self)
