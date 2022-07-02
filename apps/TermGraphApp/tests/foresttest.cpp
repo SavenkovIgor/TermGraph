42a330598eacbe8fcd6189c50d9796abf2c31c84
SavenkovIgor <savenkovigor.dev@gmail.com>
2022-07-02 22:46:45 +0300

    Spdx format license replacement

--- apps/TermGraphApp/tests/foresttest.cpp
+++ apps/TermGraphApp/tests/foresttest.cpp
@@ -1,23 +1,5 @@
-/*
- *  TermGraph, build graph of knowledge.
- *  Copyright © 2016-2020. Savenkov Igor. All rights reserved
- *  Contacts: dev.savenkovigor@protonmail.com
- *
- *  This file is part of TermGraph.
- *
- *  TermGraph is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  TermGraph is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with TermGraph. If not, see <https://www.gnu.org/licenses/>.
- */
+// Copyright © 2016-2022. Savenkov Igor
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 #include <set>
 
