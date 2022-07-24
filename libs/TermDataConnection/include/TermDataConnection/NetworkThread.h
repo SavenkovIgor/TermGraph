5b0de7973237d454239dbd3374886f07a883b6ab
SavenkovIgor <savenkovigor.dev@gmail.com>
2022-07-24 03:19:00 +0300

    Add wrappers for network calls

--- libs/TermDataConnection/include/TermDataConnection/NetworkThread.h
+++ libs/TermDataConnection/include/TermDataConnection/NetworkThread.h
@@ -1,16 +1,27 @@
 // Copyright © 2016-2022. Savenkov Igor
 // SPDX-License-Identifier: GPL-3.0-or-later
 
+#include <functional>
+
 #include <QThread>
 #include <QNetworkAccessManager>
+#include <QNetworkReply>
+
 
 class NetworkThread : public QThread
 {
 public:
+    using ReplyCallback = std::function<void(QNetworkReply* reply)>;
+
     NetworkThread();
     ~NetworkThread();
 
     void run() final;
+
+    void get(QUrl url, ReplyCallback callback) const;
+    void post(QUrl url, QByteArray data, ReplyCallback callback) const;
+    void put(QUrl url, QByteArray data, ReplyCallback callback) const;
+    void deleteResource(QUrl url, ReplyCallback callback) const;
 
     QSharedPointer<QNetworkAccessManager> manager;
 };
--- libs/TermDataConnection/source/NetworkThread.cpp
+++ libs/TermDataConnection/source/NetworkThread.cpp
@@ -3,6 +3,7 @@
 
 #include "include/TermDataConnection/NetworkThread.h"
 
+
 NetworkThread::NetworkThread()
     : QThread()
 {
@@ -15,4 +16,52 @@ void NetworkThread::run()
 {
     manager.reset(new QNetworkAccessManager());
     exec();
+}
+
+void NetworkThread::get(QUrl url, std::function<void (QNetworkReply *)> callback) const
+{
+    QMetaObject::invokeMethod(manager.get(), [this, url, callback](){
+        auto* reply = manager->get(QNetworkRequest(url));
+
+        QObject::connect(reply, &QNetworkReply::finished, [=] {
+            callback(reply);
+            reply->deleteLater();
+        });
+    });
+}
+
+void NetworkThread::post(QUrl url, QByteArray data, ReplyCallback callback) const
+{
+    QMetaObject::invokeMethod(manager.get(), [this, url, data, callback](){
+        auto* reply = manager->post(QNetworkRequest(url), data);
+
+        QObject::connect(reply, &QNetworkReply::finished, [=] {
+            callback(reply);
+            reply->deleteLater();
+        });
+    });
+}
+
+void NetworkThread::put(QUrl url, QByteArray data, ReplyCallback callback) const
+{
+    QMetaObject::invokeMethod(manager.get(), [this, url, data, callback](){
+        auto* reply = manager->put(QNetworkRequest(url), data);
+
+        QObject::connect(reply, &QNetworkReply::finished, [=] {
+            callback(reply);
+            reply->deleteLater();
+        });
+    });
+}
+
+void NetworkThread::deleteResource(QUrl url, ReplyCallback callback) const
+{
+    QMetaObject::invokeMethod(manager.get(), [this, url, callback](){
+        auto* reply = manager->deleteResource(QNetworkRequest(url));
+
+        QObject::connect(reply, &QNetworkReply::finished, [=] {
+            callback(reply);
+            reply->deleteLater();
+        });
+    });
 }
