a43d245da5add005dffcad7ed9da50fbb7febca2
SavenkovIgor <savenkovigor.dev@gmail.com>
2022-08-07 15:57:01 +0300

    One more fix

--- .build/srv_install.sh
+++ .build/srv_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-apt install -y git vim gcc-12 g++-12 cmake ninja-build python3 python3-pip3 &&
+apt install -y git vim gcc-12 g++-12 cmake ninja-build python3 python3-pip &&
 apt install -y libgl1-mesa-dev pkg-config xorg-dev libx11-xcb-dev libxcb*-dev xkb-data libfontconfig1-dev libxkbcommon-x11-dev &&
 pip3 install conan
