eb6b04a5ea5b3c70ae61bab0b6b5d1028607e4a1
Savenkov Igor <savenkovigor.dev@gmail.com>
2022-10-02 00:27:04 +0300

    Update of wasm build script (#21)

--- tools/build_qt_wasm/build/build.sh
+++ tools/build_qt_wasm/build/build.sh
@@ -1,19 +1,22 @@
+# Exec manually!
 source "/home/user/emsdk/emsdk_env.sh"
 
+# Qt root path
+QT_ROOT=$HOME/Qt
+QT_VERSION=6.4.0
+
 # cmake path
-PATH=$PATH:$HOME/Qt/Tools/CMake/bin
+PATH=$PATH:$QT_ROOT/Tools/CMake/bin
 # ninja path
-PATH=$PATH:$HOME/Qt/Tools/Ninja
+PATH=$PATH:$QT_ROOT/Tools/Ninja
 
 # CMAKE_TOOLCHAIN_FILE=/home/user/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake
 
 # CONFIG_PLATFORM=linux-g++
 CONFIG_PLATFORM=wasm-emscripten
 
-# CONFIG_HOST_PATH=/home/user/Qt/6.4.0/gcc_64
-# CONFIG_HOST_PATH=/home/user/Qt/6.4.0/wasm_32
-CONFIG_HOST_PATH=/home/user/Qt/6.3.1/gcc_64
-# CONFIG_HOST_PATH=/home/user/Qt/6.3.1/wasm_32
+CONFIG_HOST_PATH=$QT_ROOT/$QT_VERSION/gcc_64
+# CONFIG_HOST_PATH=$QT_ROOT/$QT_VERSION/wasm_32
 
 CONFIG_MODULES='qtbase,qtsvg,qtimageformats,qtshadertools,qtdeclarative'
 
@@ -22,7 +25,7 @@ git clone git://code.qt.io/qt/qt5.git ../source
 cd ../source
 
 echo "---Checkout---"
-git checkout v6.3.1
+git checkout v$QT_VERSION
 
 echo "---Init-repository---"
 perl init-repository # --module-subset=$CONFIG_MODULES
@@ -33,7 +36,7 @@ cd ../build
 THREAD_ARG='-feature-thread'
 
 echo "---Configure---"
-../source/configure -qt-host-path $CONFIG_HOST_PATH -platform $CONFIG_PLATFORM $THREAD_ARG -prefix /home/user/Qt/6.3.1/wasm_mt_build -submodules $CONFIG_MODULES
+../source/configure -qt-host-path $CONFIG_HOST_PATH -platform $CONFIG_PLATFORM $THREAD_ARG -prefix $QT_ROOT/$QT_VERSION/wasm_32_mt -submodules $CONFIG_MODULES
 
 echo "---Build---"
 cmake --build . --parallel 4
