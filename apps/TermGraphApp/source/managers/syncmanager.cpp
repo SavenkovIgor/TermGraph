b2c9c5a8b36119036a1ce69768509fc33aa2f8aa
SavenkovIgor <savenkovigor.dev@gmail.com>
2022-10-23 16:08:51 +0400

    Remove unused stuff from sync manager

--- apps/TermGraphApp/source/managers/syncmanager.cpp
+++ apps/TermGraphApp/source/managers/syncmanager.cpp
@@ -16,38 +16,4 @@ SyncManager::SyncManager(NetworkManager* networkManager, GroupsManager* groupsMa
     connect(this->mGroupsManager, &GroupsManager::exportGroupReady, this->mNetworkManager, &NetworkManager::sendGroup);
 }
 
-bool SyncManager::isDataContainer(const QJsonDocument& doc)
-{
-    bool hasContentType = false;
-    bool hasVersion     = false;
-    bool hasContent     = false;
-
-    auto docObject = doc.object();
-
-    hasContentType = docObject.contains(contentTypeFieldName);
-    hasVersion     = docObject.contains(versionFieldName);
-    hasContent     = docObject.contains(contentFieldName);
-
-    return hasContentType && hasVersion && hasContent;
-}
-
 void SyncManager::sendGroupByNetwork(const QUuid groupUuid) { mGroupsManager->requestGroupExport(groupUuid); }
-
-QString SyncManager::getContentTypeName(const SyncManager::ContentType& type)
-{
-    switch (type) {
-    case ContentType::groupsHashList: return "groupsHashList";
-    case ContentType::undefined: return "";
-    }
-
-    return "";
-}
-
-SyncManager::ContentType SyncManager::getContentTypeByName(const QString& typeName)
-{
-    if (typeName == "groupsHashList") {
-        return ContentType::groupsHashList;
-    }
-
-    return ContentType::undefined;
-}
--- apps/TermGraphApp/source/managers/syncmanager.h
+++ apps/TermGraphApp/source/managers/syncmanager.h
@@ -17,20 +17,9 @@ class SyncManager : public QObject
 public:
     explicit SyncManager(NetworkManager* networkManager, GroupsManager* groupsManger, QObject* parent = nullptr);
 
-    enum class ContentType { undefined = 0, groupsHashList };
-
-    constexpr static auto contentTypeFieldName = "contentType";
-    constexpr static auto versionFieldName     = "version";
-    constexpr static auto contentFieldName     = "content";
-
-    static bool isDataContainer(const QJsonDocument& doc);
-
     Q_INVOKABLE void sendGroupByNetwork(const QUuid groupUuid);
 
 private:
     NetworkManager* mNetworkManager;
     GroupsManager*  mGroupsManager;
-
-    static QString     getContentTypeName(const ContentType& type);
-    static ContentType getContentTypeByName(const QString& typeName);
 };
