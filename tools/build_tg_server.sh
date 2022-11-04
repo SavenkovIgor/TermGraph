40959411b26d8cc74fd70e82985ba95230c9d695
SavenkovIgor <savenkovigor.dev@gmail.com>
2022-11-04 02:02:14 +0200

    Remove root cmake list entirely

--- tools/build_tg_server.sh
+++ tools/build_tg_server.sh
@@ -1,2 +1,2 @@
-conan install -if ../conanfiles -pr ../conanfiles/profiles/linux -pr:b=default -o with_qt=True -o build_application=False --update --build=missing ../
+conan install -if ../conanfiles -pr ../conanfiles/profiles/linux -pr:b=default -o with_qt=True --update --build=missing ../
 conan build -if ../conanfiles ../
