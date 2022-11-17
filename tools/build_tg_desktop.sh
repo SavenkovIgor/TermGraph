9305593abe56e9db86f0df4386ab2277991628ee
SavenkovIgor <savenkovigor.dev@gmail.com>
2022-11-17 23:07:03 +0200

    Cleanup

--- tools/build_tg_desktop.sh
+++ tools/build_tg_desktop.sh
@@ -1,7 +1,7 @@
 cd ../apps/TermGraphApp
 
 echo "\n\n---CONAN INSTALL STARTED---\n" &&
-conan install ./ -pr conanfiles/profile/desktop_debug -g=cmake_paths -g=json -g=cmake --build=missing -if=build/dev/conan-dependencies &&
+conan install . --profile conanfiles/profile/desktop_debug --build=missing -if=build/dev/conan-dependencies &&
 
 echo "\n\n---CMAKE CONFIGURE STARTED---\n" &&
 cmake --preset dev ./ &&
