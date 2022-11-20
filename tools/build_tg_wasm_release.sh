42f57823c31dffad197abbd3ff5ba8402e3fa9c1
SavenkovIgor <savenkovigor.dev@gmail.com>
2022-11-21 00:03:42 +0200

    Draft implementation

---
+++ tools/build_tg_wasm_release.sh
@@ -0,0 +1,12 @@
+source ~/emsdk/emsdk_env.sh
+
+cd ../apps/TermGraphApp
+
+echo "\n\n---CONAN INSTALL STARTED---\n" &&
+conan install . --profile conanfiles/profile/wasm_release --build=missing -if=build/wasm_release/conan-dependencies &&
+
+echo "\n\n---CMAKE CONFIGURE STARTED---\n" &&
+cmake --preset wasm_release ./ &&
+
+echo "\n\n---CMAKE BUILD STARTED---\n" &&
+cmake --build --preset wasm_release
