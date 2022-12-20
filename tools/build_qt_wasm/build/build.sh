c0232152bbda49cc643b8924c393567ee819e68e
Savenkov Igor <savenkovigor.dev@gmail.com>
2022-12-21 01:37:52 +0200

    9 automated build for wasm (#94)
    * Create wasm_release.yml
    
    * Update wasm_release.yml
    
    * Add some output
    
    * Update wasm_release.yml
    
    * Build script start fix
    
    * Fix for build.sh
    
    * Add environment variables
    
    * Update wasm_release.yml
    
    * Update wasm_release.yml
    
    * Remove unused variable
    
    * Update wasm_release.yml
    
    * Update wasm_release.yml
    
    * Update wasm_release.yml
    
    * Update wasm_release.yml
    
    * Update wasm_release.yml
    
    * More neat syntax at presets
    
    * Cleanup in presets
    
    * Call fix
    
    * Root fix
    
    * Update wasm_release.yml
    
    * Revert "Update wasm_release.yml"
    
    This reverts commit 1785dace45b464af5d48fc29677323fcfd9d1aac.
    
    * Add Qt6_DIR capture
    
    * Update wasm_release.yml
    
    * Update build.sh
    
    * Update wasm_release.yml
    
    * Update CMakePresets.json
    
    * Update wasm_release.yml
    
    * Update wasm_release.yml
    
    * Update wasm_release.yml
    
    * Update wasm_release.yml
    
    * Update CMakePresets.json
    
    * Update wasm_release.yml
    
    * Remove of comment
    
    * Remove some info steps
    
    * Add caching try

--- tools/build_qt_wasm/build/build.sh
+++ tools/build_qt_wasm/build/build.sh
@@ -1,42 +1,69 @@
-# Exec manually!
-source "$HOME/emsdk/emsdk_env.sh"
+#!/bin/bash
 
-# Qt root path
-QT_ROOT=$HOME/Qt
-QT_VERSION=6.4.1
+variable_exist() {
+  local varname=$1
+  local example_msg=$2
+if [[ -z "${!varname}" ]]; then
+    printf "Error: variable not defined $varname\nCan be defined as: $example_msg\n"
+    exit 1
+fi
+}
 
-# cmake path
-PATH=$PATH:$QT_ROOT/Tools/CMake/bin
-# ninja path
-PATH=$PATH:$QT_ROOT/Tools/Ninja
+folder_exist() {
+  local folder_path_var=$1
+  if [[ ! -d "${!folder_path_var}" ]]; then
+    printf "Error: folder not exist at ${folder_path_var} (${!folder_path_var})\n"
+    exit 1
+  else
+    printf "Folder exist at ${folder_path_var} (${!folder_path_var})\n"
+  fi
+}
 
-# CMAKE_TOOLCHAIN_FILE=/home/user/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake
+file_exist() {
+  local file_path_var=$1
+  if [[ ! -f "${!file_path_var}" ]]; then
+    printf "Error: file not exist at ${file_path_var} (${!file_path_var})\n"
+    exit 1
+  else
+    printf "File exist at ${file_path_var} (${!file_path_var})\n"
+  fi
+}
 
-# CONFIG_PLATFORM=linux-g++
+variable_exist QT_ROOT    "~/Qt"
+variable_exist QT_VERSION "6.4.1"
+
+# If fails here, exec emscripten evn script manually!
+# source "$HOME/emsdk/emsdk_env.sh"
+variable_exist EMSDK      "~/emsdk"
+variable_exist EMSDK_NODE "~/emsdk/node/14.18.2_64bit/bin/node"
+
+QT_VERSION_PATH=$QT_ROOT/$QT_VERSION
+
+folder_exist QT_ROOT
+folder_exist QT_VERSION_PATH
+folder_exist EMSDK
+file_exist EMSDK_NODE
+
 CONFIG_PLATFORM=wasm-emscripten
-
-CONFIG_HOST_PATH=$QT_ROOT/$QT_VERSION/gcc_64
-# CONFIG_HOST_PATH=$QT_ROOT/$QT_VERSION/wasm_32
-
+CONFIG_HOST_PATH=$QT_VERSION_PATH/gcc_64
 CONFIG_MODULES='qtbase,qtsvg,qtimageformats,qtshadertools,qtdeclarative'
 
 echo "---Cloning---"
-git clone git://code.qt.io/qt/qt5.git ../source
-cd ../source
+cd ..
+git clone git://code.qt.io/qt/qt5.git ./source
+cd ./source
 
 echo "---Checkout---"
 git checkout v$QT_VERSION
 
 echo "---Init-repository---"
-perl init-repository # --module-subset=$CONFIG_MODULES
+perl init-repository
 
-cd ../build
-
-# THREAD_ARG=
 THREAD_ARG='-feature-thread'
 
 echo "---Configure---"
-./configure -qt-host-path $CONFIG_HOST_PATH -platform $CONFIG_PLATFORM $THREAD_ARG -prefix $QT_ROOT/$QT_VERSION/wasm_32_mt -submodules $CONFIG_MODULES -skip 'qtwebengine'
+rm -rf qtwebengine
+./configure -qt-host-path $CONFIG_HOST_PATH -platform $CONFIG_PLATFORM $THREAD_ARG -prefix $QT_VERSION_PATH/wasm_32_mt -submodules $CONFIG_MODULES -skip 'qtwebengine'
 
 echo "---Build---"
 cmake --build . --parallel 4
