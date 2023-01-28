4b42ac0855253d0419162d8610aa30ca13c85d6f
Savenkov Igor <savenkovigor.dev@gmail.com>
2023-01-28 22:29:44 +0200

    Remove of docker files (#126)

--- tools/docker/info.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-echo "--- IMAGES ---"
-docker image ls
-echo "--- CONTAINERS ---"
-docker container ps -a
--- tools/docker/linuxDockerBuild.sh
+++
@@ -1,7 +0,0 @@
-rm -rf ./build &&
-#conan install -if ./conan -pr ./conan/profiles/linux ./ &&
-#conan install -if ./conan -pr ./conan/profiles/linux ./ &&
-conan install -if ./conan -pr ./conan/profiles/linux --build=qt ./ &&
-mkdir -p build &&
-chmod 777 build &&
-conan build -bf ./build -if ./conan ./
