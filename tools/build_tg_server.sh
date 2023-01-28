78d30d07f13616058e1a1c6fd312db86eb7d60ef
Savenkov Igor <savenkovigor.dev@gmail.com>
2023-01-29 00:12:07 +0200

    Replace bash scripts with python (#127)
    * Replace bash scripts with python
    
    * Remove unused file
    
    * Readme fix
    
    * Remove unused env files

--- tools/build_tg_server.sh
+++
@@ -1,2 +0,0 @@
-conan install -if ../conanfiles -pr ../conanfiles/profiles/linux -pr:b=default --update --build=missing ../
-conan build -if ../conanfiles ../
--- tools/build_tg_wasm.sh
+++
@@ -1,1 +0,0 @@
-conan install -if ../conanfiles -pr ../conanfiles/profiles/linux --update --build=missing ../
