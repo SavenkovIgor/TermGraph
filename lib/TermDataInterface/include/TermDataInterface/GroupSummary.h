12d02150010d016d25b5977bbe45db9f657ec1d1
SavenkovIgor <savenkovigor.dev@gmail.com>
2023-03-17 00:58:34 +0200

    Split GroupSummary

--- lib/TermDataInterface/include/TermDataInterface/GroupSummary.h
+++ lib/TermDataInterface/include/TermDataInterface/GroupSummary.h
@@ -21,8 +21,8 @@
 struct GroupSummary
 {
     Opt<GroupUuid> uuid;
-    QString name;
-    QString comment;
+    QString        name;
+    QString        comment;
 
     int            size          = 0;
     QDateTime      lastEdit      = QDateTime();
@@ -33,105 +33,23 @@ struct GroupSummary
     inline bool operator==(const GroupSummary& rhs) const = default;
 
     // --- JSON ---
-    static inline Opt<GroupSummary> from(const QJsonObject& obj)
-    {
-        if (!GroupJsonValidator::defaultChecks().check(obj))
-            return std::nullopt;
+    static Opt<GroupSummary> from(const QJsonObject& obj);
+    static Opt<GroupSummary> from(const QByteArray& jsonBytes);
 
-        GroupSummary ret;
+    QJsonObject toQJsonObject() const;
 
-        ret.uuid     = GroupUuid::from(obj[JsonTools::uuidKey].toString());
-        ret.name     = obj[JsonTools::nameKey].toString();
-        ret.comment  = obj[JsonTools::commentKey].toString();
-        ret.size     = obj[JsonTools::sizeKey].toInt(0);
-        ret.lastEdit = QDateTime::fromString(obj[JsonTools::lastEditKey].toString(), Qt::ISODate);
-
-        auto nodeLastEdit = QDateTime::fromString(obj[JsonTools::nodesLastEditKey].toString(), Qt::ISODate);
-        if (!nodeLastEdit.isNull())
-            ret.nodesLastEdit = nodeLastEdit;
-        else
-            ret.nodesLastEdit = std::nullopt;
-
-
-        assert(!ret.isNull());
-
-        if (ret.isNull()) // Release safety
-            return std::nullopt;
-
-        return ret;
-    }
-
-    static inline Opt<GroupSummary> from(const QByteArray& jsonBytes)
-    {
-        auto doc = QJsonDocument::fromJson(jsonBytes);
-
-        if (doc.isNull())
-            return std::nullopt;
-
-        return from(doc.object());
-    }
-
-    operator QJsonObject() const
-    {
-        QJsonObject ret;
-
-        ret.insert(JsonTools::uuidKey, (uuid ? uuid->toString() : ""));
-        ret.insert(JsonTools::nameKey, name);
-        ret.insert(JsonTools::commentKey, comment);
-        ret.insert(JsonTools::sizeKey, size);
-        ret.insert(JsonTools::lastEditKey, lastEdit.toString(Qt::ISODate));
-
-        if (nodesLastEdit)
-            ret.insert(JsonTools::nodesLastEditKey, nodesLastEdit->toString(Qt::ISODate));
-
-        return ret;
-    }
-
+    explicit operator QJsonObject() const { return toQJsonObject(); }
     explicit operator QByteArray() const { return QJsonDocument(static_cast<QJsonObject>(*this)).toJson(); }
 
     class List : public std::vector<GroupSummary>
     {
     public:
-        static inline Opt<List> from(const QJsonObject& obj)
-        {
-            List ret;
+        static Opt<List> from(const QJsonObject& obj);
+        static Opt<List> from(const QByteArray& jsonBytes);
 
-            if (!obj[JsonTools::groupsKey].isArray())
-                return std::nullopt;
+        QJsonObject toQJsonObject() const;
 
-            for (const auto& groupJson : obj[JsonTools::groupsKey].toArray()) {
-                if (auto groupInfo = GroupSummary::from(groupJson.toObject())) {
-                    ret.push_back(*groupInfo);
-                } else {
-                    qWarning("Wrong groupInfo in received data");
-                }
-            }
-
-            return ret;
-        }
-
-        static inline Opt<List> from(const QByteArray& jsonBytes)
-        {
-            auto doc = QJsonDocument::fromJson(jsonBytes);
-
-            if (doc.isNull())
-                return std::nullopt;
-
-            return from(doc.object());
-        }
-
-        explicit operator QJsonObject() const
-        {
-            QJsonArray arr;
-
-            for (const auto& item : *this)
-                arr.push_back(static_cast<QJsonObject>(item));
-
-            QJsonObject obj;
-            obj.insert(JsonTools::groupsKey, arr);
-            return obj;
-        }
-
+        explicit operator QJsonObject() const { return toQJsonObject(); }
         explicit operator QByteArray() const { return QJsonDocument(static_cast<QJsonObject>(*this)).toJson(); }
     };
 };
