496893cdd2dbacb5fc31fdd29d12274103d2b9ab
Savenkov Igor <savenkovigor.dev@gmail.com>
2024-03-23 21:42:40 +0200

    Add ranges library and update assertions

--- lib/Graph/include/Graph/Graph.hpp
+++ lib/Graph/include/Graph/Graph.hpp
@@ -8,10 +8,13 @@
 #include <set>
 #include <utility>
 #include <vector>
+#include <ranges>
 
 #include <Graph/Edge.hpp>
 #include <Graph/GraphData.hpp>
 #include <Graph/Node.hpp>
+
+namespace rng = std::ranges;
 
 namespace graph {
 
@@ -37,8 +40,8 @@ public:
         std::set<NodePtr> nodeSet(data.nodes.begin(), data.nodes.end());
         std::set<EdgePtr> edgeSet(data.edges.begin(), data.edges.end());
 
-        assert(nodeSet.size() == data.nodes.size());
-        assert(edgeSet.size() == data.edges.size());
+        assert(rng::size(nodeSet) == rng::size(data.nodes));
+        assert(rng::size(edgeSet) == rng::size(data.edges));
     }
 
     const NodeList& nodeList() const { return Base::nodes; }
