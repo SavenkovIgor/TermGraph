f3d6eb64fd7ed9b895d953b2e1c21172b3b2ddb2
Savenkov Igor <savenkovigor.dev@gmail.com>
2024-03-31 15:41:13 +0300

    Move static storage to main part (#357)
    * Move static storage to main part
    
    * Add AUTORCC property to TermGraph target

--- source/StaticDataStorage/StaticDataStorage.cpp
+++ source/StaticDataStorage/StaticDataStorage.cpp
@@ -1,7 +1,7 @@
 // Copyright © 2016-2023. Savenkov Igor
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-#include "include/StaticDataStorage/StaticDataStorage.h"
+#include "source/StaticDataStorage/StaticDataStorage.h"
 
 #include <ranges>
 
