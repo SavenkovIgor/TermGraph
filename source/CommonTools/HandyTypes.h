d516da120d8f28e33ef8cf7613a06f1e821e142d
Savenkov Igor <savenkovigor.dev@gmail.com>
2025-06-07 12:40:19 +0000

    Remove Result class in favor of std::expected for error handling

--- source/CommonTools/HandyTypes.h
+++ source/CommonTools/HandyTypes.h
@@ -16,19 +16,6 @@ using UuidList = std::vector<QUuid>;
 template<typename T>
 using Expected = std::expected<T, ErrorCode>;
 
-template<typename T>
-class Result : public std::variant<T, ErrorCode>
-{
-public:
-    using std::variant<T, ErrorCode>::variant;
-
-    operator bool() const { return !std::holds_alternative<ErrorCode>(*this); }
-    bool has_value() const { return !std::holds_alternative<ErrorCode>(*this); }
-
-    T          value() const { return std::get<T>(*this); }
-    ErrorCode error() const { return std::get<ErrorCode>(*this); }
-};
-
 enum class Direction { Left, Right };
 
 enum class UuidMode { Default, Url };
