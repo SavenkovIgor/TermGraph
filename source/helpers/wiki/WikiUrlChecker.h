8903072746fae043b731f7ae11d551ec33c9dfb6
Savenkov Igor <savenkovigor.dev@gmail.com>
2025-06-10 03:40:44 +0400

    Refactor WikiUrlChecker to async

---
+++ source/helpers/wiki/WikiUrlChecker.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <QObject>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QString>
+
+#include <expected>
+#include <functional>
+#include <string>
+
+class WikiUrlChecker : public QObject
+{
+    Q_OBJECT
+
+public:
+    using Result   = std::expected<bool, std::string>;
+    using Callback = std::function<void(Result)>;
+
+    explicit WikiUrlChecker(QObject* parent = nullptr);
+
+    void check(const QString& term, Callback callback);
+
+private slots:
+    void onFinished();
+
+private:
+    QNetworkAccessManager mManager;
+    QNetworkReply*        mReply{nullptr};
+    Callback              mCallback;
+};
