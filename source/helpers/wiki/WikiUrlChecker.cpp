e01914d0fc51e034e61581c7bf147ea2a5bc532a
Savenkov Igor <savenkovigor.dev@gmail.com>
2025-06-14 23:13:26 +0000

    Refactor WikiUrlChecker to improve request handling and add unit tests for queued requests

--- source/helpers/wiki/WikiUrlChecker.cpp
+++ source/helpers/wiki/WikiUrlChecker.cpp
@@ -1,72 +1,103 @@
-#include "WikiUrlChecker.h"
+#include "source/helpers/wiki/WikiUrlChecker.h"
 
 #include <QNetworkRequest>
 #include <QUrl>
 
 WikiUrlChecker::WikiUrlChecker(QObject* parent)
     : QObject(parent)
-{}
+{
+    connect(&mTimer, &QTimer::timeout, this, &WikiUrlChecker::processQueue);
+    mTimer.setInterval(mRequestIntervalMs);
+    mTimer.setSingleShot(false);
+}
 
 WikiUrlChecker::~WikiUrlChecker()
 {
-    if (mReply) {
-        // Disconnect signals to avoid calling onFinished after destruction
-        disconnect(mReply, nullptr, this, nullptr);
-        mReply->abort();
-        mReply->deleteLater();
-        mReply = nullptr;
-        // Notify the callback that the operation was aborted due to destruction
-        if (mCallback) {
-            mCallback(std::unexpected("WikiUrlChecker destroyed before operation completed."));
+    if (mCurrentRequest) {
+        mCurrentRequest->reply->abort();
+        mCurrentRequest->reply->deleteLater();
+        mCurrentRequest->callback(std::unexpected("WikiUrlChecker destroyed before operation completed."));
+        mCurrentRequest.reset();
+    }
+    // Notify all pending requests that the checker is destroyed
+    while (!mQueue.empty()) {
+        auto& req = mQueue.front();
+        if (req.callback) {
+            req.callback(std::unexpected("WikiUrlChecker destroyed before operation completed."));
         }
+        mQueue.pop();
     }
 }
 
-void WikiUrlChecker::checkPageExist(const QString& term, Callback callback)
+void WikiUrlChecker::checkPageExists(const QString& term, Callback callback) { addToQueue(term, std::move(callback)); }
+
+void WikiUrlChecker::processQueue()
 {
-    if (mReply) {
-        // Instead of silent failure, immediately invoke the callback with an error
-        if (callback) {
-            callback(std::unexpected("WikiUrlChecker is busy: another check is already in progress."));
-        }
+    if (isBusy() || mQueue.empty()) {
         return;
     }
+    mCurrentRequest = std::move(mQueue.front());
+    mQueue.pop();
 
-    mCallback = std::move(callback);
+    QUrl url(QString("https://en.wikipedia.org/wiki/%1").arg(mCurrentRequest->term));
 
-    QString encodedTerm = term;
-    encodedTerm.replace(' ', '_');
-    QUrl url(QString("https://en.wikipedia.org/wiki/%1").arg(encodedTerm));
     QNetworkRequest request(url);
-
-    mReply = mManager.head(request);
-    connect(mReply, &QNetworkReply::finished, this, &WikiUrlChecker::onFinished);
+    mCurrentRequest->reply = mManager.head(request);
+    connect(mCurrentRequest->reply, &QNetworkReply::finished, this, &WikiUrlChecker::onFinished);
 }
 
 void WikiUrlChecker::onFinished()
 {
-    auto* reply = mReply;
-    mReply = nullptr;
-
-    Result result;
+    auto* reply = mCurrentRequest->reply;
+    CheckResult result;
 
     if (reply->error() == QNetworkReply::ContentNotFoundError) {
         result = false;
     } else if (reply->error() != QNetworkReply::NoError) {
-        result = std::unexpected(reply->errorString().toStdString());
+        result = std::unexpected("Failed to check Wikipedia page '" +
+                                mCurrentRequest->term.toStdString() +
+                                "': " + reply->errorString().toStdString());
     } else {
         int status = reply->attribute(QNetworkRequest::HttpStatusCodeAttribute).toInt();
-        result = (status == 200);
+        result     = (status == 200);
     }
 
     reply->deleteLater();
 
-    if (mCallback) {
-        mCallback(std::move(result));
+    if (mCurrentRequest->callback) {
+        mCurrentRequest->callback(std::move(result));
+    }
+    mCurrentRequest.reset();
+
+    // After finishing, process the next item if available
+    stopTimerIfNeeded();
+}
+
+void WikiUrlChecker::startTimerIfNeeded()
+{
+    if (mTimer.isActive()) {
+        return;
+    }
+
+    if (!mQueue.empty()) {
+        mTimer.start();
     }
 }
 
-bool WikiUrlChecker::isBusy() const
+void WikiUrlChecker::stopTimerIfNeeded()
 {
-    return mReply != nullptr;
+    if (mQueue.empty() && !isBusy()) {
+        mTimer.stop();
+    }
+}
+
+size_t WikiUrlChecker::queueSize() const
+{
+    return mQueue.size();
+}
+
+void WikiUrlChecker::addToQueue(const QString& term, Callback&& callback)
+{
+    mQueue.push(Request{term, std::move(callback)});
+    startTimerIfNeeded();
 }
--- source/helpers/wiki/WikiUrlChecker.h
+++ source/helpers/wiki/WikiUrlChecker.h
@@ -4,30 +4,49 @@
 #include <QNetworkReply>
 #include <QObject>
 #include <QString>
+#include <QTimer>
 
 #include <expected>
 #include <functional>
 #include <string>
+#include <queue>
 
 class WikiUrlChecker : public QObject
 {
     Q_OBJECT
 
 public:
-    using Result   = std::expected<bool, std::string>;
-    using Callback = std::function<void(Result)>;
+    using CheckResult = std::expected<bool, std::string>;
+    using Callback = std::function<void(CheckResult)>;
 
     explicit WikiUrlChecker(QObject* parent = nullptr);
     ~WikiUrlChecker() override;
 
-    void checkPageExist(const QString& term, Callback callback);
-    bool isBusy() const;
+    void checkPageExists(const QString& term, Callback callback);
+    bool isBusy() const { return mCurrentRequest.has_value(); }
+    size_t queueSize() const;
 
 private slots:
     void onFinished();
+    void processQueue();
 
 private:
-    QNetworkAccessManager mManager;
-    QNetworkReply*        mReply{nullptr};
-    Callback              mCallback;
+    struct Request
+    {
+        QString        term;
+        Callback       callback;
+        QNetworkReply* reply = nullptr;
+    };
+    std::queue<Request> mQueue;
+    std::optional<Request> mCurrentRequest;
+
+    QTimer               mTimer;
+    constexpr static int mRequestIntervalMs = 1000;
+
+    QNetworkAccessManager  mManager;
+
+    void startTimerIfNeeded();
+    void stopTimerIfNeeded();
+
+    void addToQueue(const QString& term, Callback&& callback);
 };
