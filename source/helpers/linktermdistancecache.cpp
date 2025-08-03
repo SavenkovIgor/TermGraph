079c32a212d19be92de307ed92d6da27674ce4e1
Savenkov Igor <savenkovigor.dev@gmail.com>
2025-08-03 17:39:52 +0000

    Remove std namespace prefixes in LinkTermDistanceCache class

--- source/helpers/linktermdistancecache.cpp
+++ source/helpers/linktermdistancecache.cpp
@@ -7,29 +7,28 @@ module;
 
 export module LinkTermDistanceCache;
 
+using namespace std;
+
 export class LinkTermDistanceCache
 {
 public:
     LinkTermDistanceCache() = default;
 
-    std::optional<std::optional<int>> get(const QString& link, const QString& term) const
+    optional<optional<int>> get(const QString& link, const QString& term) const
     {
         auto linkIter = linkToTermCache.constFind(link);
         if (linkIter != linkToTermCache.constEnd()) {
             auto termIter = linkIter.value().constFind(term);
             if (termIter != linkIter.value().constEnd()) {
-                return std::optional<std::optional<int>>(termIter.value());
+                return optional<optional<int>>(termIter.value());
             }
         }
 
-        return std::nullopt;
+        return nullopt;
     }
 
-    void add(const QString& link, const QString& term, std::optional<int> value)
-    {
-        linkToTermCache[link][term] = value;
-    }
+    void add(const QString& link, const QString& term, optional<int> value) { linkToTermCache[link][term] = value; }
 
 private:
-    QMap<QString, QMap<QString, std::optional<int>>> linkToTermCache;
+    QMap<QString, QMap<QString, optional<int>>> linkToTermCache;
 };
