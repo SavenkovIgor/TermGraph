4d29968a158e24212f45a47e2fff5ad3e43755af
Savenkov Igor <savenkovigor.dev@gmail.com>
2025-08-10 00:02:13 +0000

    Modularize Stemmer class

--- source/helpers/stemming/Stemmer.cpp
+++ source/helpers/stemming/Stemmer.cpp
@@ -1,19 +1,32 @@
-#include "source/helpers/stemming/Stemmer.h"
+// Copyright © 2016-2025. Savenkov Igor
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+module;
+
+#include <memory>
+#include <QString>
 
 #include <english_stem.h>
 #include <russian_stem.h>
 
-using namespace stemming;
+export module Stemmer;
 
-namespace {
+export enum class StemmingLanguage { English, Russian };
+
+class Stemmer
+{
+public:
+    virtual ~Stemmer()                              = default;
+    virtual QString stem(const QString& word) const = 0;
+};
 
 class EnglishStemmer : public Stemmer
 {
 public:
     QString stem(const QString& word) const override
     {
-        std::wstring               tmp = word.toStdWString();
-        english_stem<std::wstring> st;
+        std::wstring                         tmp = word.toStdWString();
+        stemming::english_stem<std::wstring> st;
         st(tmp);
         return QString::fromWCharArray(tmp.c_str());
     }
@@ -24,16 +37,14 @@ class RussianStemmer : public Stemmer
 public:
     QString stem(const QString& word) const override
     {
-        std::wstring               tmp = word.toStdWString();
-        russian_stem<std::wstring> st;
+        std::wstring                         tmp = word.toStdWString();
+        stemming::russian_stem<std::wstring> st;
         st(tmp);
         return QString::fromWCharArray(tmp.c_str());
     }
 };
 
-} // namespace
-
-std::unique_ptr<Stemmer> create_stemmer(StemmingLanguage lang)
+export std::unique_ptr<Stemmer> create_stemmer(StemmingLanguage lang)
 {
     switch (lang) {
     case StemmingLanguage::English: return std::make_unique<EnglishStemmer>();
