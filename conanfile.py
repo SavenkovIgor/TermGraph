1f8ef8d69177ea4b244653d15c03d8c8f31237fa
Savenkov Igor <savenkovigor.dev@gmail.com>
2026-03-11 23:22:17 +0000

    build(conan): update ninja version to 1.13.2

--- conanfile.py
+++ conanfile.py
@@ -12,7 +12,7 @@ class TermGraphConan(ConanFile):
         return self.settings.os == 'Emscripten'
 
     def build_requirements(self):
-        self.tool_requires('ninja/1.11.1')
+        self.tool_requires('ninja/1.13.2')
         if not self.is_wasm():
             self.test_requires('gtest/1.17.0')
 
